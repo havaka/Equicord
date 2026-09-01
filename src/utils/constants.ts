89ef19bccc4213228dace72082cca26a928e4870
havaka <braganecegor@gmail.com>
2026-09-01 22:12:18 +0300

    dfgdfg

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -67,6 +67,10 @@ export interface Dev {
  * If you are fine with attribution but don't want the badge, add badge: false
  */
 export const Devs = /* #__PURE__*/ Object.freeze({
+    havaka: {
+        name: "havaka",
+        id: 389448357237751820n
+    },
     Ven: {
         name: "V",
         id: 343383572805058560n
